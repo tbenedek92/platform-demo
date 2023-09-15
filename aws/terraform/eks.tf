30d9a4f32aaf65df48bfec0320e23aa3ff6efc4b
Benedek Toth <t.benedek92@gmail.com>
2023-09-15 19:09:34 +0200

    Added terraform files for creating an AWS EKS cluster.

---
+++ aws/terraform/eks.tf
@@ -0,0 +1,13 @@
+resource "aws_eks_cluster" "this" {
+  name     = local.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.private[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster,
+    aws_route_table_association.public  # Ensuring the networking is ready before EKS
+  ]
+}
