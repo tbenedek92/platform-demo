476abb9998f6f50703d1d1ea340e5e4a4412097b
Benedek Toth <t.benedek92@gmail.com>
2023-09-27 15:03:43 +0200

    Updated cluster version to 1.28

--- aws/terraform/modules/eks/eks.tf
+++ aws/terraform/modules/eks/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.28"
 
   cluster_endpoint_private_access = true # was false when running
   cluster_endpoint_public_access  = true  # was true when running succesfully
