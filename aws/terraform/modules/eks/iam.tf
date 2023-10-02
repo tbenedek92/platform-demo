6a217a20081f5235bdb8e488474a25d99f8efcf8
Benedek Toth <t.benedek92@gmail.com>
2023-10-02 22:58:54 +0200

    Fix source URL in iam.tf

--- aws/terraform/modules/eks/iam.tf
+++ aws/terraform/modules/eks/iam.tf
@@ -3,7 +3,7 @@
 # Create the "allow-eks-terraform-admin" IAM policy with the "eks:DescribeCluster" action is to grant the necessary permissions for managing an Amazon EKS cluster##
 
 module "iam_policy_eks_admin_access" {
-  source        = "terraform-aws-modules/iam/aws/modules/iam-policy"
+  source        = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version       = "5.22.0"
   name          = "allow-eks-terraform-admin"
   create_policy = true
