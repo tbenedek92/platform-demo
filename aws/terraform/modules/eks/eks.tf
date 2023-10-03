4fd464295fea5cbba818a86f12b2d0bc63b31137
Benedek Toth <t.benedek92@gmail.com>
2023-10-03 23:42:47 +0200

    Revert changes to eks.tf

--- aws/terraform/modules/eks/eks.tf
+++ aws/terraform/modules/eks/eks.tf
@@ -59,20 +59,20 @@ module "eks" {
   tags = var.eks_tags
 }
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name ]
-#     command     = "aws"
-#   }
-# }
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name ]
+    command     = "aws"
+  }
+}
 
 
 # security group to enable kubeseal
