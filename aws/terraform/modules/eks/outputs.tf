81a7c6fcd83d3413abfb657debbee717db349e09
Benedek Toth <t.benedek92@gmail.com>
2023-10-03 19:43:20 +0200

    Remove unused output from eks module

--- aws/terraform/modules/eks/outputs.tf
+++ aws/terraform/modules/eks/outputs.tf
@@ -1,7 +1,3 @@
 output "eks_managed_node_groups" {
   value = module.eks.eks_managed_node_groups["general"]
 }
-
-output "eks_managed_cluster_vpc_config" {
-  value = module.eks.cluster_security_group_id
-}
