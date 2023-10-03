deffbeb4c13b0e20bb8fb3f195bc719714ddd015
Benedek Toth <t.benedek92@gmail.com>
2023-10-03 23:47:18 +0200

    Add variables and values to the eks_cluster module in main.tf

--- aws/terraform/environments/prd/main.tf
+++ aws/terraform/environments/prd/main.tf
@@ -1,3 +1,11 @@
 module "eks_cluster" {
   source = "../../modules/eks"
+  
+  aws_region = var.aws_region
+  cluster_name = var.cluster_name
+  eks_tags = var.eks_tags
+  node_group_min_size = var.node_group_min_size
+  node_group_max_size = var.node_group_max_size
+  node_group_desired_size = var.node_group_desired_size
+  network_name = var.network_name
 }
