1d8c223a75918e3249aa632304c40a982d892a2d
Benedek Toth <t.benedek92@gmail.com>
2023-10-03 19:46:45 +0200

    Adding security group rule for 8080 (kubeseal) (#23)
     Add security group rule to enable kubeseal on port 8080
    (squash merging due to experimentations)

---
+++ aws/terraform/environments/prd/outputs.tf
@@ -0,0 +1,4 @@
+output "eks_managed_node_group_general_attributes" {
+  description = "Attributes of the 'general' managed node group"
+  value       = module.eks_cluster
+}
--- aws/terraform/modules/eks/eks.tf
+++ aws/terraform/modules/eks/eks.tf
@@ -73,3 +73,15 @@ provider "kubernetes" {
     command     = "aws"
   }
 }
+
+
+# security group to enable kubeseal
+resource "aws_security_group_rule" "allow_eks_cluster_to_node_on_8080" {
+  type        = "ingress"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+
+  security_group_id = module.eks.node_security_group_id
+  source_security_group_id = module.eks.cluster_security_group_id
+}
